bd7e4c98ac876cfc944f1c6e90d858bf5cc7eb88
Cesar Valdez <cesar.valdez@feedhenry.com>
2015-05-27 20:34:11 +0100

    Transformation class finished

--- www/demos/triangle.js
+++ www/demos/triangle.js
@@ -5,9 +5,35 @@
   var shader = new VR8.Shader();
   var frag   = document.getElementById('fragment-shader').innerHTML;
   var vert   = document.getElementById('vertex-shader').innerHTML;
-  var transform = Object.create(VR8.transform);
-  var camera    = transform.ortho(0,50,50,0,1,-1);
+  var camera    = VR8.Camera.MakeOrtho(0,50,50,0,1,-1);
   var scene     = new VR8.Scene2D();
+  var pos = {x:0, y:0};  
+  var mov = {x:0, y:0}; 
+  var zoomOut = false; 
+ 
+
+  a.addEventListener('click', function(){ zoomOut = true;});
+
+  a.addEventListener('mousemove', function(evt) {
+   var tmp =  getMousePos(a,evt,{x: VR8.W, y:VR8.H} );
+   console.log(pos);
+   
+   if(tmp){
+   
+    if(tmp.x > pos.x)
+        mov.x=-0.03;
+    else 
+        mov.x=0.03;
+   
+    if(tmp.y > pos.y)
+        mov.y=-0.03;
+    else
+        mov.y=0.03;
+   }
+
+    pos = tmp;
+        
+  });
 
   shader.link(vert, frag);
   
@@ -25,35 +51,23 @@
     scene.camera = camera;
   }();
   
-  function create_vertex(v7){
-      var buff = new Float32Array(7);
-      if(typeof v7.pos !== 'undefined'){
-        buff.set(v7.pos.get(), 0);
-      }else
-          throw 'missing vector3 for position.';
-      
-      if(typeof v7.color !== 'undefined') { 
-        buff.set(v7.color.get(), 3); 
-      }else
-        buff.set([0.7,0.7,0.7,0.7], 3); //set to white as default.
-      return buff;   
-  }
-
   var line_triangle = VR8.geometry.mesh(20000000);
 
   var triangle = function(){
     var buffer = new VR8.Buffer();
     buffer.no_color_data = false;
     return {
-        cord: function(p1, p2,p3){
-            line_triangle.add( new Vector(  p1.x,  p1.y, 0.0) ); 
-            line_triangle.add( new Vector(  p2.x,  p2.y, 0.0) ); 
+        cord: function(p1, p2,p3, deep){
+            var vec_gray = new Vector(0.6,0.7,0.7);
+            deep = deep * 0.0001;
+            line_triangle.add( new Vector(  p1.x,  p1.y, 0.0),vec_gray.multiplyByScalar(deep)  ); 
+            line_triangle.add( new Vector(  p2.x,  p2.y, 0.0),vec_gray.multiplyByScalar(deep)   ); 
             
-            line_triangle.add( new Vector(  p1.x,  p1.y, 0.0) ); 
-            line_triangle.add( new Vector(  p3.x,  p3.y, 0.0) ); 
+            line_triangle.add( new Vector(  p1.x,  p1.y, 0.0), vec_gray.multiplyByScalar(deep)  ); 
+            line_triangle.add( new Vector(  p3.x,  p3.y, 0.0),vec_gray.multiplyByScalar(deep)   ); 
             
-            line_triangle.add( new Vector(  p3.x,  p3.y, 0.0) ); 
-            line_triangle.add( new Vector(  p2.x,  p2.y, 0.0) );
+            line_triangle.add( new Vector(  p3.x,  p3.y, 0.0), vec_gray.multiplyByScalar(deep) ); 
+            line_triangle.add( new Vector(  p2.x,  p2.y, 0.0), vec_gray.multiplyByScalar(deep)  );
             return this;
         },
         buffer: function(){ 
@@ -61,6 +75,8 @@
             return buffer; }
     }
   }();
+
+  var level = 0;
 var sierpinski = function(p1, p2, p3, limit){
         if(limit >0){
           var pa = {
@@ -82,22 +98,43 @@ var sierpinski = function(p1, p2, p3, limit){
           sierpinski(pa,p2,pb, limit-1);
           sierpinski(pc,pb,p3, limit-1);
         }else{
-          triangle.cord(p1,p2,p3);
+            level ++;
+          triangle.cord(p1,p2,p3,level);
         }
   };
   
-  sierpinski({x: 0.0, y: -24.0 }, {x: -20.0, y:20.0}, {x:20.0, y: 20.0}, 5);
-
+  sierpinski({x: 0.0, y: -24.0 }, {x: -20.0, y:20.0}, {x:20.0, y: 20.0}, 9);
+  var t = new VR8.Transform(); 
+  t.translate(25,25).scale(1,1,0);
   var entity = {
     buffer: triangle.buffer(),
-    model: transform.translate(25,25,0).m(),
+    model: t.m,
     drawType: 'LINES'
   }
 
   function init(){
   }
-
+  var p = {x: 0, y:0};
+  var f = new Vector(0.5,0.5,0);
+  var zoomx1 = new Vector(1.0,1.0,0.0); 
+  var step = new Vector(0.006, 0.006, 0.006); 
+  var tm = 0; 
+  var rot = 0.1;
   function update(){
+      p.x += mov.x; 
+      p.y += mov.y;
+      rot += 1;
+     // t.scale(f.v[0],f.v[1],0);
+      t.rotateY(rot); ////.translate(25+p.x,25+p.y,0);
+      f.add(step);
+ 
+      if(zoomOut){
+        tm+= 0.001; 
+        VR8.Lerp(f,zoomx1, tm);
+        if(tm >= 1){  zoomOut = false; tm=0;} 
+      }  
+
+
   }
   
   function render(){
--- www/js/math/transform.js
+++ www/js/math/transform.js
@@ -1,3 +1,5 @@
+VR8.Camera = VR8.Camera || {};
+
 /*
  * [ 0 4  8 12 ]   [ 0 4  8 12 ]
  * [ 1 5  9 13 ] x [ 1 5  9 13 ]
@@ -7,6 +9,67 @@
  *
  * */
 
+VR8.Transform = function(_m){
+    var _mm = Math; 
+    var mtx = _m?_m.m():new Float32Array([1.0,0.0,0.0,0.0,  0.0,1.0,0.0,0.0,  0.0,0.0,1.0,0.0, 0.0,0.0,0.0,1.0 ]);
+    this.m = mtx;
+    
+    this.scale = function(x,y,z){ mtx[0]=x; mtx[5]=y; mtx[10]=z; return this;}
+    this.translate = function(x,y,z){ mtx[12]=x; mtx[13]=y; mtx[14]=z; return this;}
+    this.rotateX = function(angle){
+        var radian = angle * _mm.PI/180; 
+        mtx[5] =  _mm.cos(radian); 
+        mtx[9] = -_mm.sin(radian);
+        mtx[6] =  _mm.sin(radian); 
+        mtx[10] =  _mm.cos(radian);
+        return this;
+    };
+     
+    this.rotateY = function(angle){
+        var radian = angle * _mm.PI/180; 
+        mtx[0] =  _mm.cos(radian); 
+        mtx[8] = -_mm.sin(radian);
+        mtx[2] =  _mm.sin(radian); 
+        mtx[10] =  _mm.cos(radian);
+        return this;
+    }; 
+
+  this.rotateZ = function(angle){
+        var radian = angle * _mm.PI/180; 
+        mtx[0] =  _mm.cos(radian); 
+        mtx[4] = _mm.sin(radian);
+        mtx[1] = -_mm.sin(radian); 
+        mtx[5] =  _mm.cos(radian);
+        return this;
+    }; 
+
+
+
+
+}
+
+VR8.Camera.MakeOrtho = function(left, right, bottom, top, nearz, farz  )
+{
+    var m = new Float32Array(16); 
+    
+    m[1] = m[2] = m[3] = m[4] = m[6] 
+    = m[7]=m[8]=m[9]=m[11] = 0.0;
+
+    m[0] = 2.0 / (right - left ); 
+    m[5] = 2.0 / (top - bottom)
+    m[12] = -(right + left) / (right - left);
+    m[13] = -(top + bottom) / (top - bottom);
+    m[14] = -(farz + nearz) / (farz - nearz);
+    m[15] = 1.0;
+    return m;
+}
+    
+    
+    
+    
+
+
+/*
 VR8.transform = {
   _mm: Math,
 
@@ -70,4 +133,6 @@ VR8.transform = {
                         _mm.sin(a), _mm.cos(a) ]);
     return mat4;
   }
-};
+};i
+
+*/
