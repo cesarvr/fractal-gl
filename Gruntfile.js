9d83f447af6abc316900cac0494bcdd7a376d6eb
Cesar Valdez <cesar.valdez@feedhenry.com>
2015-07-11 17:38:06 +0100

    fixes

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,16 +1,55 @@
 module.exports = function(grunt) {
 
-grunt.loadNpmTasks('grunt-serve');
+    grunt.loadNpmTasks('grunt-serve');
+    grunt.loadNpmTasks('grunt-contrib-jade');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compass');
 
-grunt.initConfig({
-    serve: {
-        options: {
-            port: 9000,
-             'serve': {
-     	   		'path': 'www'
-    		}
+    grunt.initConfig({
+        compass: {
+            dev: { 
+                options: {
+                    sassDir: 'www/sass',
+                    cssDir: 'www/stylesheets'
+                }
+            }
+        },
+        serve: {
+            options: {
+                port: 9000,
+                'serve': {
+                    'path': '.'
+                }
+            }
+        },
+
+        jade: {
+            compile: {
+                options: {
+                    data: {
+                        debug: false
+                    }
+                },
+                files: {
+                    "index.html": ["www/jade/*.jade"]
+                }
+            }
+        },
+
+        watch: {
+            templates: {
+                files: ['www/jade/*.*'],
+                tasks: ['jade'],
+            },
+            sass: {
+                files: ['www/sass/*.scss'],
+                tasks: ['compass']
+            }
         }
-    }
-});
 
+
+    });
+
+
+    grunt.registerTask('default', ['serve', 'watch']);
 };
