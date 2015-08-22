f1fab350cc644ad7e9881dac23de7bf067124d52
Cesar Valdez <cesar@MacBook-Pro-de-Cesar.local>
2015-08-22 19:24:05 +0100

    initial

---
+++ js/core/buffer.js
@@ -0,0 +1,57 @@
+VR8.Buffer = function() {
+    var gl = VR8.webGL;
+
+    this.buffer = null;
+    this.render_type = gl.STATIC_DRAW;
+    this.size = 0;
+    this.stride = 0;
+    this.color_size = 4;
+    this.vertex_size = 3;
+    this.no_color_data = true;
+
+    this.geometry = function(g) {
+        this.buffer = gl.createBuffer();
+        gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
+        gl.bufferData(
+            gl.ARRAY_BUFFER,
+            new Float32Array(g.points),
+            this.render_type
+        );
+
+        this.size = g.size;
+        this.sides = g.points.length / g.size;
+        this.stride = g.size * Float32Array.BYTES_PER_ELEMENT;
+    }
+
+
+    this.update = function(g) {
+        gl.bufferData(
+            gl.ARRAY_BUFFER,
+            new Float32Array(g.points),
+            this.render_type
+        );
+
+    }
+
+    this.upload_vertex = function(shader_position) {
+        gl.vertexAttribPointer(shader_position,
+            this.vertex_size,
+            gl.FLOAT,
+            false,
+            this.stride,
+            0);
+    }
+
+    this.upload_colors = function(shader_color) {
+        if (this.no_color_data)
+            return;
+
+        gl.vertexAttribPointer(shader_color,
+            this.color_size,
+            gl.FLOAT,
+            false,
+            this.stride,
+            this.vertex_size * Float32Array.BYTES_PER_ELEMENT
+        );
+    }
+}
---
+++ js/utils/util.js
@@ -0,0 +1,31 @@
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
+
+function getMousePos(canvas, evt, viewport) {
+	var rect = canvas.getBoundingClientRect();
+	var x = evt.clientX - rect.left;
+    var y = evt.clientY - rect.top;
+
+    x = viewport.x * x / rect.right;
+    y = viewport.y * y / rect.bottom;
+    return {
+	    x: x,
+		y: y
+	};
+}
+
+function randomN(val){
+
+ return  Math.floor(Math.random() * val) + 1
+}
+
+function randomF(val){
+
+ return (Math.random() * (0.000120 - val) + val);
+}
